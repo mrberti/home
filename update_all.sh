f83f76c239299b5662e592eeeb241d0ad28999e1
Simon Bertling <simon.bertling@gmx.de>
2018-01-23 23:39:50 +0900

    improved update scripts

---
+++ update_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script needs to be run from its own directory, so cd into it
+fullpath=`realpath $0`
+dirname=`dirname $fullpath`
+cd $dirname
+
+if [ ! -e "files.txt" ]; then
+	echo "File 'filex.txt' does not exist."
+	exit 1
+fi
+
+for file in `cat files.txt`; do
+	
+	if [ -e $file ]; then
+		echo $file
+		diff -u $file ../$file
+	else
+		echo next...
+	fi
+done
+#cp `sed 's!^!'"$HOME"'/!' < files.txt` .
